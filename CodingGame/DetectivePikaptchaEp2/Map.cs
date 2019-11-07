2416231543c1f090f1be972dd0a0b099571afea7
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-11-07 22:03:48 +0100

    PIkaptcha V2

---
+++ CodingGame/DetectivePikaptchaEp2/Map.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingGame.DetectivePikaptchaEp2
+{
+    public class Map
+    {
+        private int[,] Cells;
+        private int Width;
+        private int Height;
+        private Pikaptcha Pikaptcha;
+        public Map(int width, int height)
+        {
+            this.Cells = new int[width, height];
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public void SetPikaptcha(int i, int j, char face)
+            => this.Pikaptcha = new Pikaptcha(i, j, face);
+
+        public void SetWallOn(string direction)
+            => this.Pikaptcha.SetWallOn(direction);
+
+        public void Zero(int i, int j)
+            => this.Cells[i, j] = int.MinValue;
+
+        public void Play()
+        {
+            this.Cells[this.Pikaptcha.X, this.Pikaptcha.Y]++;
+            this.Play(this.Pikaptcha.X, this.Pikaptcha.Y);
+        }
+
+        private void Play(int x, int y)
+        {
+            (int X, int Y) result = this.Pikaptcha.Move(x, y, this.Cells, this.Width, this.Height);
+            if (result.X == this.Pikaptcha.X && result.Y == this.Pikaptcha.Y)
+                this.Show();
+            else
+            {
+                this.Cells[result.X, result.Y]++;
+                this.Play(result.X, result.Y);
+            }
+        }
+
+        public void Show()
+        {
+            for (int j = 0; j < this.Height; j++)
+            {
+                StringBuilder end = new StringBuilder();
+                for (int i = 0; i < this.Width; i++)
+                {
+                    end.Append(this.Cells[i, j] < 0 ? "#" : this.Cells[i, j].ToString());
+                }
+                Console.WriteLine(end.ToString());
+            }
+        }
+    }
+}
---
+++ CodingGame/DetectivePikaptchaEp2/Pikaptcha.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingGame.DetectivePikaptchaEp2
+{
+    public class Pikaptcha
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Face Face { get; private set; }
+        public WallOn WallOn { get; private set; }
+        public Pikaptcha(int x, int y, Face face)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Face = face;
+        }
+        public Pikaptcha(int x, int y, char face) : this(x, y, FromString(face))
+        {
+           
+        }
+        private static Face FromString(char face)
+        {
+            switch (face)
+            {
+                default:
+                case '>':
+                    return Face.Right;
+                case 'v':
+                    return Face.Down;
+                case '<':
+                    return Face.Left;
+                case '^':
+                    return Face.Up;
+            }
+        }
+        public void SetWallOn(string value)
+        {
+            if (value == "L")
+                this.WallOn = WallOn.Left;
+            else
+                this.WallOn = WallOn.Right;
+        }
+        public (int X, int Y) Move(int x, int y, int[,] cells, int width, int height)
+        {
+            int count = 0;
+            (int X, int Y, Face face) next;
+            Face face = this.Face;
+            do
+            {
+                if (count > 0)
+                    face = face.Rotate();
+                next = face.Next(this.WallOn, x, y);
+                count++;
+            } while (count < 4 && (next.X < 0 || next.Y < 0 || next.X >= width || next.Y >= height || cells[next.X, next.Y] < 0));
+            this.Face = next.face;
+            return (next.X, next.Y);
+        }
+    }
+    public static class FaceExtensions
+    {
+        public static (int X, int Y, Face Face) Next(this Face face, WallOn wallOn, int x, int y)
+        {
+            switch (face)
+            {
+                default:
+                case Face.Right:
+                    if (wallOn == WallOn.Left)
+                        return (x, y - 1, Face.Up);
+                    else
+                        return (x, y + 1, Face.Down);
+                case Face.Left:
+                    if (wallOn == WallOn.Left)
+                        return (x, y + 1, Face.Down);
+                    else
+                        return (x, y - 1, Face.Up);
+                case Face.Down:
+                    if (wallOn == WallOn.Left)
+                        return (x + 1, y, Face.Right);
+                    else
+                        return (x - 1, y, Face.Left);
+                case Face.Up:
+                    if (wallOn == WallOn.Left)
+                        return (x - 1, y, Face.Left);
+                    else
+                        return (x + 1, y, Face.Right);
+            }
+        }
+        public static Face Rotate(this Face face)
+        {
+            switch (face)
+            {
+                case Face.Down:
+                    return Face.Left;
+                case Face.Left:
+                    return Face.Up;
+                case Face.Up:
+                    return Face.Right;
+                default:
+                case Face.Right:
+                    return Face.Down;
+            }
+        }
+    }
+    public enum Face
+    {
+        Right,
+        Down,
+        Left,
+        Up
+    }
+    public enum WallOn
+    {
+        Left,
+        Right
+    }
+}
