1f9ab333d44e3f4eb5341711e757ae04cb0f26c8
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-12-13 08:13:43 +0100

    Goup

---
+++ CodingGame/Brackets/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingGame.Brackets
+{
+    internal class Solution
+    {
+        internal void Run()
+        {
+            char[] a = new char[2];
+        }
+    }
+}
---
+++ CodingGame/Plague Jr/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Plague_Jr
+{
+    internal class Human
+    {
+        public int Id { get; }
+        public List<int> Next { get; private set; }
+        public bool Infected { get; private set; } = false;
+        public Human(int id, int next)
+        {
+            this.Id = id;
+            this.Next = new List<int>() { next };
+        }
+        public void AddFurtherNext(int next)
+        {
+            if (!this.Next.Contains(next))
+                this.Next.Add(next);
+        }
+        public void Infect()
+            => this.Infected = true;
+        public override string ToString()
+            => $"{this.Id} -> {string.Join(",", this.Next)}";
+    }
+}
