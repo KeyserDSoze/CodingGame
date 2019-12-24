3f6542847681382d8f5de92c1ee827973874a545
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-12-24 19:05:38 +0100

    Count_as_I_count

---
+++ CodingGame/Count as I count/Result.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Count_as_I_count
+{
+    internal class Result
+    {
+        public int X { get; private set; }
+        public List<int> Shoots { get; } = new List<int>();
+        public int Combination
+        {
+            get
+            {
+                int combination = GetCombination(this.Shoots);
+                for (int i = 0; i < this.Shoots.Count; i++)
+                {
+                    List<int> shoots = new List<int>();
+                    foreach (int shoot in Shoots)
+                        shoots.Add(shoot);
+                    for (int j = i; j < shoots.Count; j++)
+                    {
+                        if (shoots[j] > 1)
+                        {
+                            shoots[j] += 40;
+                            combination += GetCombination(shoots);
+                        }
+                    }
+                }
+                return combination;
+            }
+        }
+        private int GetCombination(List<int> values)
+        {
+            Dictionary<int, int> counted = new Dictionary<int, int>();
+            foreach (int x in this.Shoots)
+            {
+                if (!counted.ContainsKey(x))
+                    counted.Add(x, 0);
+                counted[x]++;
+            }
+            int divisor = 1;
+            foreach (var x in counted)
+                divisor *= Factorial(x.Value);
+            return Factorial(values.Count) / divisor;
+        }
+        private int Factorial(int value)
+        {
+            int result = 1;
+            for (int i = 2; i <= value; i++)
+                result *= i;
+            return result;
+        }
+        public bool Won => this.X == 50;
+        public Result(int n, int x)
+        {
+            this.X = n + x;
+            this.Shoots.Add(x);
+        }
+        public void NewShoot(int x)
+        {
+            if (this.X + x <= 50)
+            {
+                this.Shoots.Add(x);
+                this.X += x;
+            }
+        }
+        public override string ToString()
+            => $"{this.X} {string.Join("|",this.Shoots)}";
+    }
+}
---
+++ CodingGame/Count as I count/Solution.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodinGame.Count_as_I_count
+{
+    internal class Solution : ICodinGame
+    {
+        public void Run()
+        {
+            int n = 38;
+            if (n < 2 || n >= 50)
+            {
+                Console.WriteLine("0");
+            }
+            else if (n == 49)
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.Error.WriteLine(n);
+                List<Result> results = new List<Result>();
+                for (int round = 1; round <= 4; round++)
+                {
+                    for (int i = 1; i <= 12; i++)
+                    {
+                        if (round > 1)
+                        {
+                            List<Result> morePossibilities = new List<Result>();
+                            foreach (Result result in results.Where(x => !x.Won))
+                            {
+                                if (result.Shoots.Count < round)
+                                    result.NewShoot(i);
+                                else
+                                {
+                                    Result result1 = new Result(n, result.Shoots.First());
+                                    foreach (int shoot in result.Shoots.Skip(1).Take(result.Shoots.Count - 2))
+                                        result1.NewShoot(shoot);
+                                    result1.NewShoot(i);
+                                    morePossibilities.Add(result1);
+                                }
+                            }
+                            results.AddRange(morePossibilities);
+                        }
+                        else if (n + i <= 50)
+                        {
+                            results.Add(new Result(n, i));
+                        }
+                    }
+                }
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+                Console.Error.WriteLine(string.Join(",", results.Where(x => x.Won)));
+                Console.WriteLine(results.Where(x => x.Won).Sum(x => x.Combination));
+            }
+        }
+    }
+}
