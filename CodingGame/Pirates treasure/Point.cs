3216ccffe83b2af8cd7b6cdaa0d1678e96f0bdf7
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2019-12-24 17:05:59 +0100

    Pirate's treasure

---
+++ CodingGame/Pirates treasure/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Pirates_treasure
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Value { get; }
+        public bool IsValid { get; private set; }
+
+        public Point(int x, int y, int value)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Value = value;
+            this.IsValid = value == 0;
+        }
+
+        public void ChangeValidity()
+            => this.IsValid = false;
+
+    }
+}
---
+++ CodingGame/Pirates treasure/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Pirates_treasure
+{
+    internal class Solution : ICodinGame
+    {
+        static List<string> Inputs = new List<string>()
+        {
+            "1 1 1 0",
+            "1 0 1 0",
+            "1 1 1 1",
+            "0 0 1 1"
+        };
+        static int H = Inputs.Count;
+        static int W = Inputs[0].Length / 2 + 1;
+        public void Run()
+        {
+            Point[,] points = new Point[H, W];
+            for (int i = 0; i < H; i++)
+            {
+                string[] inputs = Inputs[i].Split(' ');
+                for (int j = 0; j < W; j++)
+                {
+                    int value = int.Parse(inputs[j]);
+                    points[i, j] = new Point(i, j, value);
+                }
+            }
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    if (points[i, j].IsValid && points[i, j].Value == 0)
+                    {
+                        for (int x = i == 0 ? 0 : i - 1; x <= i + 1 && x < H; x++)
+                        {
+                            for (int y = j == 0 ? 0 : j - 1; y <= j + 1 && y < W; y++)
+                            {
+                                if (!(x == i && y == j))
+                                {
+                                    points[x, y].ChangeValidity();
+                                    if (points[x, y].Value == 0)
+                                        points[i, j].ChangeValidity();
+                                }
+                            }
+                        }
+                        if (points[i, j].IsValid)
+                        {
+                            Console.WriteLine($"{j} {i}");
+                            goto End;
+                        }
+                    }
+                }
+            }
+        End: Console.Error.WriteLine("End");
+        }
+    }
+}
