aa02e36b4a036db3608e0c6cad55098c2470c722
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-01-08 01:25:44 +0100

    Number of letters in a number - Binary

---
+++ CodingGame/Medium/Number of letters in a number - Binary/Phrase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Medium.Number_of_letters_in_a_number___Binary
+{
+    internal class Phrase
+    {
+        public int Value { get; }
+        public Phrase(long value)
+        {
+            foreach (char c in Convert.ToString(value, 2))
+                this.Value += c == '0' ? 4 : 3;
+        }
+        public Phrase Next() 
+            => new Phrase(this.Value);
+    }
+}
---
+++ CodingGame/Medium/Number of letters in a number - Binary/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Medium.Number_of_letters_in_a_number___Binary
+{
+    internal class Solution : ICodinGame
+    {
+        public void Run()
+        {
+            const long start = 99999999999999999;
+            const long n = 99999999999999999;
+            Phrase phrase = new Phrase(start);
+            long last = long.MinValue;
+            for (long i = 0; i < n - 1; i++)
+            {
+                phrase = phrase.Next();
+                if (last == phrase.Value)
+                    break;
+                last = phrase.Value;
+            }
+            Console.WriteLine(phrase.Value);
+        }
+    }
+}
