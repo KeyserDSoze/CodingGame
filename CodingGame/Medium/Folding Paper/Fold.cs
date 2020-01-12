a9b64e88381f4c0682a8d1b67097c4dc8dfaf6e2
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-01-12 23:30:03 +0100

    Folding Paper

---
+++ CodingGame/Medium/Folding Paper/Fold.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Medium.Folding_Paper
+{
+    internal class Fold
+    {
+        public int Up { get; private set; } = 1;
+        public int Down { get; private set; } = 1;
+        public int Left { get; private set; } = 1;
+        public int Right { get; private set; } = 1;
+        public void UpBlending()
+        {
+            this.Down += this.Up;
+            this.Left *= 2;
+            this.Right *= 2;
+            this.Up = 1;
+        }
+        public void DownBlending()
+        {
+            this.Up += this.Down;
+            this.Left *= 2;
+            this.Right *= 2;
+            this.Down = 1;
+        }
+        public void LeftBlending()
+        {
+            this.Right += this.Left;
+            this.Up *= 2;
+            this.Down *= 2;
+            this.Left = 1;
+        }
+        public void RightBlending()
+        {
+            this.Left += this.Right;
+            this.Up *= 2;
+            this.Down *= 2;
+            this.Right = 1;
+        }
+        public int FromThisPointOfView(string side)
+        {
+            switch (side)
+            {
+                case "U":
+                    return this.Up;
+                case "D":
+                    return this.Down;
+                case "L":
+                    return this.Left;
+                case "R":
+                    return this.Right;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ CodingGame/Medium/Folding Paper/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Medium.Folding_Paper
+{
+    class Solution : ICodinGame
+    {
+        static string order = "UL";
+        static string side = "D";
+        public void Run()
+        {
+            Fold fold = new Fold();
+            foreach(char c in order)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        fold.UpBlending();
+                        break;
+                    case 'D':
+                        fold.DownBlending();
+                        break;
+                    case 'L':
+                        fold.LeftBlending();
+                        break;
+                    case 'R':
+                        fold.RightBlending();
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            Console.WriteLine(fold.FromThisPointOfView(side));
+        }
+    }
+}
