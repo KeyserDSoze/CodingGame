7e385925ea723c2976010b5280480d558ca08cfc
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-01-12 03:39:47 +0100

    OneWay City

---
+++ CodingGame/Medium/OneWay City/BigNumber.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Medium.OneWay_City
+{
+    internal class BigNumber
+    {
+        public int[] Value { get; }
+        public int this[int x] => this.Value[x];
+        public int Length
+        {
+            get
+            {
+                for (int i = 0; i < this.Value.Length; i++)
+                    if (this.Value[i] > 0)
+                        return this.Value.Length - i;
+                return 0;
+            }
+        }
+        public BigNumber(int value, int max = 1000)
+        {
+            this.Value = new int[max];
+            for (int i = max - 1; i >= 0; i--)
+            {
+                int remainder = value % 10;
+                this.Value[i] = remainder;
+                value = value / 10;
+                if (value == 0)
+                    break;
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            bool firstValue = false;
+            for (int i = 0; i < this.Value.Length; i++)
+                if (firstValue || (firstValue = this.Value[i] > 0))
+                    stringBuilder.Append(this.Value[i]);
+            string value = stringBuilder.ToString();
+            return value == string.Empty ? "0" : value;
+        }
+        public static BigNumber Zero => new BigNumber(0);
+        public static BigNumber One => new BigNumber(1);
+        public BigNumber Factorial(BigNumber min = null)
+        {
+            if (min == null)
+                min = new BigNumber(2);
+            BigNumber c = new BigNumber(1);
+            BigNumber last = this + One;
+            for (BigNumber i = min; i < last; i += One)
+                c *= i;
+            return c;
+        }
+        public static BigNumber operator +(BigNumber a, BigNumber b)
+        {
+            BigNumber c = new BigNumber(0);
+            int next = 0;
+            for (int i = b.Value.Length - 1; i >= 0; i--)
+            {
+                int value = a[i] + b[i] + next;
+                int remainder = value % 10;
+                c.Value[i] = remainder;
+                next = value / 10;
+            }
+            return c;
+        }
+        public static BigNumber operator -(BigNumber a, BigNumber b)
+        {
+            BigNumber c = new BigNumber(0);
+            int next = 0;
+            for (int i = b.Value.Length - 1; i >= 0; i--)
+            {
+                int value = a[i] - b[i] - next;
+                if (value < 0)
+                {
+                    value += 10;
+                    next = 1;
+                }
+                else
+                    next = 0;
+                c.Value[i] = value;
+            }
+            return c;
+        }
+        public static bool operator <(BigNumber a, BigNumber b)
+        {
+            int aLength = a.Length;
+            int bLength = b.Length;
+            if (aLength < bLength)
+                return true;
+            else if (aLength > bLength)
+                return false;
+            else
+            {
+                for (int i = b.Length; i >= 0; i--)
+                {
+                    int aValue = a[a.Value.Length - 1 - i];
+                    int bValue = b[b.Value.Length - 1 - i];
+                    if (aValue < bValue)
+                        return true;
+                    else if (aValue > bValue)
+                        return false;
+                }
+                return false;
+            }
+        }
+        public static bool operator >(BigNumber a, BigNumber b)
+        {
+            int aLength = a.Length;
+            int bLength = b.Length;
+            if (aLength > bLength)
+                return true;
+            else if (aLength < bLength)
+                return false;
+            else
+            {
+                for (int i = b.Length; i >= 0; i--)
+                {
+                    int aValue = a[a.Value.Length - 1 - i];
+                    int bValue = b[b.Value.Length - 1 - i];
+                    if (aValue > bValue)
+                        return true;
+                    else if (aValue < bValue)
+                        return false;
+                }
+                return false;
+            }
+        }
+        public static BigNumber operator *(BigNumber a, BigNumber b)
+        {
+            BigNumber c = new BigNumber(0);
+            int aLength = a.Length;
+            int bLength = b.Length;
+            int[] indexes = new int[aLength];
+            for (int i = 0; i < aLength; i++)
+                indexes[i] = -1 * i;
+            int next = 0;
+            int round = a.Length + b.Length;
+            for (int i = 0; i < round; i++)
+            {
+                int value = next;
+                for (int j = 0; j < aLength; j++)
+                {
+                    if (indexes[j] >= 0 && indexes[j] < bLength)
+                        value += a[a.Value.Length - 1 - j] * b[b.Value.Length - 1 - indexes[j]];
+                    indexes[j]++;
+                }
+                int remainder = value % 10;
+                c.Value[c.Value.Length - 1 - i] = remainder;
+                next = value / 10;
+            }
+            return c;
+        }
+        public static BigNumber operator /(BigNumber a, BigNumber b)
+        {
+            int count = 0;
+            BigNumber c = a;
+            while(c > b)
+            {
+                c -= b;
+                count++;
+            }
+            return new BigNumber(count + 1);
+        }
+    }
+}
---
+++ CodingGame/Medium/OneWay City/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace CodinGame.Medium.OneWay_City
+{
+    class Solution : ICodinGame
+    {
+        static int M = 71;
+        static int N = 75;
+        public void Run()
+        {
+            string value = GetValue(M, N).ToString();
+            Console.WriteLine(value.Length > 1000 ? value.Substring(0, 1000) : value);
+            //BigNumber a = new BigNumber(999);
+            //BigNumber b = new BigNumber(99);
+            //Console.Error.WriteLine(a > b);
+            //Console.Error.WriteLine(a < b);
+            //Console.Error.WriteLine(a.Factorial(b));
+            //Console.Error.WriteLine(a / b);
+        }
+        static BigInteger GetValue(int width, int height)
+        {
+            int min = (width > height ? height : width) - 1;
+            int max = (width > height ? width : height) - 1;
+            return (width + height - 2).Factorial(max) / min.Factorial();
+        }
+        static BigNumber GetValue(int width, int height, bool tro)
+        {
+            BigNumber min = new BigNumber((width > height ? height : width) - 1);
+            BigNumber max = new BigNumber((width > height ? width : height) - 1);
+            return new BigNumber(width + height - 2).Factorial(max) / min.Factorial();
+        }
+    }
+    static class IntExtensions
+    {
+        public static BigInteger Factorial(this int x, int min = 0)
+        {
+            BigInteger result = 1;
+            for (int i = 2; i <= x; i++)
+            {
+                if (i > min)
+                    result *= i;
+            }
+            return result;
+        }
+    }
+}
