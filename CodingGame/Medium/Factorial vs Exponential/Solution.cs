12db074ea0c2e49dc7e3196617a88c78474daa96
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-01-13 08:32:23 +0100

    Factorial_vs_Exponential

--- CodingGame/Medium/Factorial vs Exponential/Solution.cs
+++ CodingGame/Medium/Factorial vs Exponential/Solution.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Numerics;
 
 namespace CodinGame.Medium.Factorial_vs_Exponential
 {
@@ -9,16 +9,106 @@ namespace CodinGame.Medium.Factorial_vs_Exponential
     {
         static List<string> Inputs = new List<string>()
         {
-            "36",
-            "57.64",
-            "92.73",
-            "14.95",
-            "49",
-            "11",
-            "6",
-            "76.9",
-            "93.83",
-            "80",
+           "3027.64",
+            "4734.53",
+            "2306",
+            "4316.74",
+            "8091.79",
+            "4385.96",
+            "4827",
+            "2811",
+            "637.86",
+            "7584.03",
+            "6471",
+            "9751.4",
+            "662",
+            "6590",
+            "2227.08",
+            "3006",
+            "1450",
+            "3876.51",
+            "6503.87",
+            "5029",
+            "8968",
+            "8161",
+            "7743",
+            "6146.37",
+            "7705.2",
+            "191.38",
+            "8412",
+            "7968.66",
+            "6349.78",
+            "5197",
+            "3792",
+            "5640.61",
+            "2191",
+            "4057",
+            "7511.5",
+            "75.71",
+            "9716",
+            "2338",
+            "5359.63",
+            "4196",
+            "1812",
+            "1497",
+            "6500",
+            "5265.09",
+            "7861",
+            "5994.93",
+            "5212",
+            "4687",
+            "7350.89",
+            "6615",
+            "7840.03",
+            "2046.5",
+            "5702",
+            "3069",
+            "1574",
+            "2783",
+            "6864.35",
+            "5444",
+            "3843",
+            "7986.35",
+            "2514",
+            "94",
+            "1343.43",
+            "6928",
+            "1581",
+            "8429",
+            "2684",
+            "2226.15",
+            "7182.02",
+            "1517.36",
+            "6527.87",
+            "7380.3",
+            "324",
+            "3870",
+            "3349",
+            "5935",
+            "2108",
+            "1512.28",
+            "7593",
+            "2642",
+            "2671.62",
+            "29.97",
+            "530.35",
+            "96",
+            "5305",
+            "2241",
+            "3117",
+            "2415",
+            "1016.67",
+            "2277.83",
+            "4269",
+            "5471",
+            "1195.57",
+            "1018",
+            "5434.34",
+            "9266.84",
+            "4872.7",
+            "2785",
+            "935.94",
+            "4895.59",
         };
         public void Run()
         {
@@ -33,35 +123,35 @@ namespace CodinGame.Medium.Factorial_vs_Exponential
             foreach (float a in floats)
             {
                 int start = (int)a + 1;
-                bool greater = IsGreater(a, start);
-                while (!greater)
-                {
-                    start++;
-                    greater = IsGreater(a, start);
-                }
-                result += $"{start} ";
+                result += $"{WhenIsGreater(a, start * 2)} ";
             }
             Console.WriteLine(result.Trim());
         }
-        public static bool IsGreater(float a, int n)
-            => Pow(a, n) < Factorial(n);
-        public static BigInteger Pow(float a, int n)
+        public static int WhenIsGreater(float a, int n)
         {
-            BigInteger result = new BigInteger(a);
-            BigInteger aa = new BigInteger(a);
-            for (int i = 0; i < n - 1; i++)
+            const double threshold = 200000;
+            double result = a;
+            int next = n;
+            List<int> integers = new List<int>();
+            for (int i = n; i >= 2; i--)
+                integers.Add(i);
+            while (integers.Count > 0)
             {
-                result *= aa;
+                int i = result < 1 ? integers.Last() : integers.First();
+                result *= a / i;
+                integers.Remove(i);
+                while (result > threshold)
+                {
+                    next++;
+                    result *= a / next;
+                }
             }
-            return result;
-
-        }
-        public static BigInteger Factorial(int n)
-        {
-            BigInteger product = 1;
-            for (int i = 2; i <= n; i++)
-                product *= i;
-            return product;
+            while (result > 1)
+            {
+                next++;
+                result *= a / next;
+            }
+            return next;
         }
     }
 }
