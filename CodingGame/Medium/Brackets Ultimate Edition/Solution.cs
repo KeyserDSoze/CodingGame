23d1e30aedf479044fb66c9ca63cfd3a7889d538
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-04-13 02:25:04 +0200

    Palindromic Decomposition

--- CodingGame/Medium/Brackets Ultimate Edition/Solution.cs
+++ CodingGame/Medium/Brackets Ultimate Edition/Solution.cs
@@ -7,31 +7,37 @@ namespace CodinGame.Medium.Brackets_Ultimate_Edition
 {
     internal class Solution : ICodinGame
     {
-        static Regex Regex = new Regex(@"[^\(\)\[\]\{\}<>]*");
-        const string B1d = "((";
-        const string B2d = "[[";
-        const string B3d = "{{";
-        const string B4d = "<<";
-        const char B1o = '(';
-        const char B1c = ')';
-        const char B2o = '[';
-        const char B2c = ']';
-        const char B3o = '{';
-        const char B3c = '}';
-        const char B4o = '<';
-        const char B4c = '>';
+        private readonly static Regex Regex = new Regex(@"[^\(\)\[\]\{\}<>]*");
+        private static readonly Dictionary<string, int> PossibleValues = new Dictionary<string, int>()
+        {
+            { "()", 0},
+            { "[]", 0},
+            { "{}", 0},
+            { "<>", 0},
+            { "((", 1},
+            { "))", 1},
+            { "[[", 1},
+            { "]]", 1},
+            { "{{", 1},
+            { "}}", 1},
+            { "<<", 1},
+            { ">>", 1},
+            { "][", 2},
+            { ")(", 2},
+            { "}{", 2},
+            { "><", 2},
+        };
         static List<string> Inputs = new List<string>()
         {
-            "<{[(abc(]}>",
+            "ShH3xi678soQJ9KxLG4NLgD7Rx3uMpAOe53SPVsWswOful8iaOh5RV7FAZfnQPBNH7X2iUvP2XNTC36nO9Gb5hxXY{wzn2KuaEaKzyQ3DLRMaQ2u5pbe5J7LLgf0G6PlyZC6xiDBSlXvpCpE7Utqokvx5iOL8IWP2LIbNIeP3KGOK3Izvtx{azS2RzPRg4RsTbDtYkzgChFVNiJSTAttHdpjq4b61sOkjCKNHKyLfe9RTrXR6jhJoDXfAyc6Soss1E4eHo9MDaVik}mNS4YvEeVeJhyYev1m1RS1yWSz30zCbIz09h2B2r1qcLjcoK5VICSn3j2yHpRLUf2z0dZisKlj7fOALjYnlxR82FP}8ZyVBOKUfCcnaFsbKK6KyntbXegwrc4uJ8MZKm24wMMnx1ooXVjE4Ltmyp0p1BLCPIfDrJCNkwmBtQ3SwoJptb4f4(7BJwu03HekY346YiRR09nthQgp2yE1sFbODYy1gog4AncGef7JZf5M5FjKisg0l5vpCRqkqYBTtaVZxGklDu92prq(EisaLdeXxIMOfWPxnESeW0RnrlytfqEiyjdFFXeujSJNUQ7YxkBEDW5Xq8b220sd6MjwZYAnqIEYd6a6vxcKe1Pi6[Xkut2OpgtFF9sOKwr8Fsp7D0AG5XUYA1mHmsflpIR83t6nF0hrbYzX6AuvpSU4nbI99bM4U3jblXSKxWoP8sT1ZbI]fQiZQkrEijqO3BWDNCGG1gw5j5lQ3QIzlhN0Oqs9TKiU2PVryXgIkRuPA0puxcw23Dwje4quuF0hhggKl1zKJCjOL)Gx9YnlHIzOp6bd70uY0HvGk4mNgXnamkeobRlipjWn3RhMoall0YxNn7KVu20BLfvCVN7dutrAzR5RcOQ5TCbZf3E(QPdLtdtZAKEyeoX137YNoD9Emu5ur3cNOIbFR4mK4kCkiJiHJe6wRE7vSvpv953VH7hiA135MTS6F67xbJsh6Upn4",
             "<{[(abc>}])"
         };
-        private static int GetOccurencies(string start, string end)
+        private static (string Value, int Count) Contains(string input)
         {
-            int count = 0;
-            for (int i = 0; i < start.Length; i++)
-                if (start[i] != end[i])
-                    count++;
-            return count;
+            foreach (var s in PossibleValues)
+                if (input.Contains(s.Key))
+                    return (s.Key, s.Value);
+            return (string.Empty, -1);
         }
         public void Run()
         {
@@ -39,25 +45,31 @@ namespace CodinGame.Medium.Brackets_Ultimate_Edition
             for (int j = 0; j < N; j++)
             {
                 string initial = Inputs[j];
+                int count = 0;
                 initial = Regex.Replace(initial, string.Empty);
-                string value = initial;
-                initial = initial.Replace(B1c, B1o).Replace(B2c, B2o).Replace(B3c, B3o).Replace(B4c, B4o);
-                int occurrency = GetOccurencies(initial, value);
-                Console.Error.WriteLine(initial);
-                while (initial != string.Empty)
+                while (!string.IsNullOrWhiteSpace(initial))
                 {
-                    if (initial.Contains(B1d) || initial.Contains(B2d) || initial.Contains(B3d) || initial.Contains(B4d))
-                        initial = initial.Replace(B1d, string.Empty).Replace(B2d, string.Empty).Replace(B3d, string.Empty).Replace(B4d, string.Empty);
-                    else
+                    var c = Contains(initial);
+                    if (c.Count == -1)
                         break;
+                    initial = initial.ReplaceFirst(c.Value, string.Empty);
+                    count += c.Count;
                 }
                 if (initial.Length > 0)
                     Console.WriteLine("-1");
                 else
-                {
-
-                }
+                    Console.WriteLine(count);
             }
+        }
+    }
+    internal static class StringExtensionMethods
+    {
+        public static string ReplaceFirst(this string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+                return text;
+            return $"{text.Substring(0, pos)}{replace}{text.Substring(pos + search.Length)}";
         }
     }
 }
---
+++ CodingGame/Medium/Palindromic Decomposition/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodinGame.Medium.Palindromic_Decomposition
+{
+    internal class Solution : ICodinGame
+    {
+        private const string Input = "abab";
+        public void Run()
+        {
+            string S = Input;
+            Console.WriteLine(Counts(S));
+        }
+        public static int Counts(string input)
+        {
+            int count = 0;
+            for (int i = 0; i <= input.Length; i++)
+            {
+                string a = input.Substring(0, i);
+                if (a.IsPalindrome())
+                {
+                    string newValue = input.Substring(i);
+                    for (int j = 0; j <= newValue.Length; j++)
+                    {
+                        string b = newValue.Substring(0, j);
+                        if (b.IsPalindrome())
+                        {
+                            string c = newValue.Substring(j);
+                            if (c.IsPalindrome())
+                                count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+    internal static class StringExtensions
+    {
+        public static bool IsPalindrome(this string value)
+        {
+            int middle = value.Length / 2;
+            if (middle == 0)
+                return true;
+            for (int i = 0; i < middle; i++)
+                if (value[i] != value[value.Length - i - 1])
+                    return false;
+            return true;
+        }
+    }
+}
