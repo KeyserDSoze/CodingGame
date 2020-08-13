be725586276493f938ba063e5cef818ee55150c9
Alessandro Rapiti <a.rapiti@vetrya.com>
2020-08-13 18:10:50 +0200

    Easy.Van_Eck_s_sequence

---
+++ CodingGame/Easy/Van Eck's sequence/Series.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodinGame.Easy.Van_Eck_s_sequence
+{
+    internal class Series
+    {
+        private readonly List<int> Serie = new List<int>();
+        private readonly Dictionary<int, int> ComparedNumbers = new Dictionary<int, int>();
+        public Series(int start, int n)
+        {
+            this.Serie.Add(start);
+            int count = 1;
+            while (count != n)
+            {
+                int last = this.Serie.Last();
+                if (ComparedNumbers.ContainsKey(last)) 
+                {
+                    Serie.Add(count - ComparedNumbers[last]);
+                    ComparedNumbers[last] = count;
+                }
+                else
+                {
+                    Serie.Add(0);
+                    ComparedNumbers.Add(last, count);
+                }
+                count++;
+            }
+        }
+
+        public int Last()
+            => this.Serie.Last();
+    }
+}
---
+++ CodingGame/Easy/Van Eck's sequence/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodinGame.Easy.Van_Eck_s_sequence
+{
+    internal class Solution : ICodinGame
+    {
+        static int[] Input = new int[2] { 0, 1000000 };
+        public void Run()
+        {
+            int A1 = Input[0];
+            int N = Input[1];
+
+            Console.WriteLine(new Series(A1, N).Last());
+        }
+    }
+}
