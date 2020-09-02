c1c3ca3fb079d35994b0d585b3dfa5239d1b096e
Alessandro Rapiti <alessandro.rapiti44@gmail.com>
2020-09-02 12:06:31 +0200

    super computer first solution

---
+++ CodingGame/Hard/Super Computer/Day.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodinGame.Hard.Super_Computer
+{
+    class Day
+    {
+        public int From { get; }
+        public int To { get; }
+        public int Distance { get; }
+        public Day(int from, int distance)
+        {
+            this.From = from;
+            this.To = from + distance - 1;
+            this.Distance = distance;
+        }
+        public static void Next(List<Day> days)
+        {
+            List<Day> toRemove = new List<Day>();
+            Day theChoosenOne = days.First();
+            toRemove.Add(theChoosenOne);
+            foreach(Day day in days)
+            {
+                if (day.From <= theChoosenOne.To)
+                    toRemove.Add(day);
+                else
+                    break;
+            }
+            foreach(Day removed in toRemove)
+                days.Remove(removed);
+        }
+        public override string ToString()
+            => $"{this.From}-{this.To} ({this.Distance})";
+    }
+}
---
+++ CodingGame/Hard/Super Computer/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace CodinGame.Hard.Super_Computer
+{
+    class Solution : ICodinGame
+    {
+        private List<string> Inputs = new List<string>()
+        {
+           "3 5",
+            "9 2",
+            "24 5",
+            "16 9",
+            "11 6",
+        };
+        public void Run()
+        {
+            int N = Inputs.Count;
+            Dictionary<int, Day> daysAsDictionary = new Dictionary<int, Day>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Inputs[i].Split(' ');
+                int J = int.Parse(inputs[0]);
+                int D = int.Parse(inputs[1]);
+                Day d = new Day(J, D);
+                if (daysAsDictionary.ContainsKey(J))
+                {
+                    if (d.Distance < daysAsDictionary[J].Distance)
+                        daysAsDictionary[J] = d;
+                }
+                else
+                    daysAsDictionary.Add(J, d);
+            }
+
+            List<Day> days = daysAsDictionary.Select(x => x.Value).OrderBy(x => x.Distance).ToList();
+            int count = 1;
+            Day next = days.First();
+            int[] all = new int[int.MaxValue];
+            foreach (Day day in days.Skip(1))
+            {
+                if (day.From > next.To)
+                {
+                    next = day;
+                    count++;
+                }
+            }
+            while (days.Count > 0)
+            {
+                Day.Next(days);
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
